e094072795f328cf6d19da73e56ad7a353c22c65
Mateus <mateuclash@gmail.com>
2024-12-23 18:27:24 -0300

    arrumando detalhes na tela calendario feed, são eles, a questão do filtro, as agendas passadas e a rolagem de scroll

--- src/app/calendario-feed/calendario-feed/calendario-feed.component.ts
+++ src/app/calendario-feed/calendario-feed/calendario-feed.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarioUtilsComponent } from '../../calendario-utils/calendario-utils.component';
 import { GetAgendaCalendarioFeedService } from '../../services/get-agenda-calendario-feed/get-agenda-calendario-feed.service';
@@ -17,6 +17,10 @@ export class CalendarioFeedComponent {
   cidadeId: number = 9240;
 
   agendas: any[] = [];
+  filteredAgendas = [...this.agendas];
+  selectedFilter: string = 'Todos';
+
+  @ViewChild('divListaAgenda') divListaAgenda: ElementRef | undefined;
 
   constructor(private getAgendaCalendarioFeedService: GetAgendaCalendarioFeedService,
               private utilsService: UtilsService
@@ -26,11 +30,19 @@ export class CalendarioFeedComponent {
     
   }
 
+  ngAfterViewInit(): void {
+    // Verificar se a rolagem até a última agenda já passada deve ser feita
+    this.scrollToLastPassedAgenda();
+  }
+
+
   //Serviços -------------------------------------------------------------------------------------
   getCalendarioAgendas(){
     this.getAgendaCalendarioFeedService.getAgendaCalendarioFeed(this.cidadeId, this.dateSelected).subscribe({
       next: (response) => {
         this.agendas = response.calendario_hora;
+        this.filteredAgendas = [...this.agendas];
+        this.scrollToLastPassedAgenda();
       },
       error: (error) => {
         console.error("Erro ao gerar as agendas: ", error);
@@ -39,10 +51,90 @@ export class CalendarioFeedComponent {
   }
 
   //Funções ---------------------------------------------------------------------------------------
+
+  filtroLista(filtro: string){
+    this.selectedFilter = filtro;
+    if (filtro === 'Todos') {
+      this.filteredAgendas = [...this.agendas];
+      setTimeout(() => {
+        this.scrollToLastPassedAgenda();
+      }, 100);
+    } else {
+      this.filteredAgendas = this.agendas.filter(agenda => agenda.evento_nome === filtro); 
+      setTimeout(() => {
+        this.scrollToLastPassedAgenda();
+      }, 100);
+    }
+  }
+
   onDateSelected(date: string): void {
     this.dateSelected = date;
-    console.log('Data selecionada:', this.dateSelected);
     this.getCalendarioAgendas();
+    this.selectedFilter = 'Todos';
+
+    setTimeout(() => {
+      this.scrollToLastPassedAgenda();
+    }, 500);
+  }
+
+  eventoPassado(agenda: any): boolean {
+    const arrHora = agenda.agenda_hora.split(":");
+    const arrData = this.dateSelected.split("-");  // Usando a data selecionada pelo usuário
+  
+    // Criação de uma data completa para o evento com ano, mês, dia e hora
+    const eventoData = new Date(
+      parseInt(arrData[0]),   // Ano (data selecionada)
+      parseInt(arrData[1]) - 1, // Mês (lembrando que o mês no JavaScript começa de 0)
+      parseInt(arrData[2]),   // Dia (data selecionada)
+      parseInt(arrHora[0]),   // Hora do evento
+      parseInt(arrHora[1]),   // Minutos do evento
+      0                       // Segundos
+    );
+  
+    const dataAtual = new Date(); // Data e hora atuais
+  
+    return dataAtual > eventoData;  // Verifica se o evento já passou
+  }
+
+  scrollToLastPassedAgenda() {
+    // Verifica se existem agendas para o dia selecionado
+    if (this.filteredAgendas.length === 0) return;
+  
+    // Verifica se todos os eventos já passaram ou se todos são futuros
+    const todosPassados = this.filteredAgendas.every((agenda) => this.eventoPassado(agenda));
+    const todosFuturos = this.filteredAgendas.every((agenda) => !this.eventoPassado(agenda));
+  
+    // Se todos os eventos forem futuros, rola para o topo
+    if (todosFuturos) {
+      if (this.divListaAgenda) {
+        // Rolagem suave para o topo da lista
+        this.divListaAgenda.nativeElement.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    }
+    // Se todos os eventos forem passados, rola para o fim da lista
+    else if (todosPassados) {
+      if (this.divListaAgenda) {
+        // Rolagem suave para o fim da lista
+        this.divListaAgenda.nativeElement.scrollTo({ top: this.divListaAgenda.nativeElement.scrollHeight, behavior: 'smooth' });
+      }
+    }
+    // Se houver mistura de eventos passados e futuros, rola até o último evento passado
+    else {
+      const lastPassedIndex = this.filteredAgendas
+        .slice() // Faz uma cópia para evitar modificação da lista original
+        .reverse() // Reverte para começar a busca do último evento
+        .findIndex((agenda) => this.eventoPassado(agenda));
+  
+      if (lastPassedIndex !== -1 && this.divListaAgenda) {
+        // Obtém o índice correto considerando a lista invertida
+        const realIndex = this.filteredAgendas.length - 1 - lastPassedIndex;
+        const agendaElement = this.divListaAgenda.nativeElement.children[realIndex];
+  
+        if (agendaElement) {
+          agendaElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }
+    }
   }
 
   removeUf(cidade: string){
